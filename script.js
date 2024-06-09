7a9c48b6bbe9949e04a6668a74970bd0f74927b9
mervynleong <changpeng66@gmail.com>
2024-06-09 17:24:00 +0800

    adasds

--- script.js
+++ script.js
@@ -6,20 +6,52 @@ function initializeButton() {
     button.addEventListener("click", handleClick);
 }
 
-function handleClick(count) {
-    count = clickCount++;
+function handleClick() {
+    clickCount++; // Increment clickCount on every click
 
-    if (count === 1) {
-        button.removeEventListener("click", handleClick);
-        button.addEventListener("click", handleFirstClick);
-    } else if (count === 2) {
-        button.removeEventListener("click", handleFirstClick);
-        button.addEventListener("click", handleSecondClick);
+    switch (clickCount) {
+        case 1:
+            handleFirstClick();
+            break;
+        case 2:
+            handleSecondClick();
+            break;
+        case 3:
+            handleThirdClick();
+            break;
+            case 4:
+                handleForthClick();
+                break;
+                case 5:
+                    handleFifthClick();
+                    break;
+                    case 6:
+                        handleSixthClick();
+                        break;
+                        case 7:
+                            handleSeventhClick();
+                            break;
+                            case 8:
+                handleEighthClick();
+                break;
+                case 9:
+                    handleNinthClick();
+                    break;
+                    case 10:
+                        handleTenthClick();
+                        break;
+                        case 11:
+                            handleEleventhClick();
+                            break;
+                            case 12:
+                handleTwelvethClick();
+                break;
+                                                                
+
+        // Add more cases for additional click counts if needed
+        default:
+            break;
     }
-    // } else if (count===3) {
-    //     button.removeEventListener("click", handleSecondClick);
-    //     button.addEventListener("click", handleThirdClick);
-    // }// Add more conditions for additional click counts if needed
 }
 
 function handleFirstClick() {
@@ -39,8 +71,69 @@ function handleThirdClick() {
 
 }
 
-// Call the initializeButton function to set up the initial event listener
-initializeButton();
+function handleForthClick() {
+    console.log("forth click event");
+    replaceContent4();
+
+}
+
+function handleFifthClick() {
+    console.log("fifth click event");
+    replaceContent5();
+
+}
+
+function handleSixthClick() {
+    console.log("Sixth click event");
+    replaceContent6();
+
+}
+
+function handleSeventhClick() {
+    console.log("Seventh click event");
+    replaceContent7();
+
+}
+
+function handleEighthClick() {
+    console.log("Eighth click event");
+    replaceContent8();
+
+}
+
+function handleNinthClick() {
+    console.log("Ninth click event");
+    replaceContent9();
+
+}
+
+function handleTenthClick() {
+    console.log("Tenth click event");
+    replaceContent10();
+
+}
+
+function handleEleventhClick() {
+    console.log("Eleventh click event");
+    replaceContent11();
+
+}
+
+function handleTwelvethClick() {
+    console.log("Twelveth click event");
+    replaceContent12();
+    clickCount =0;
+        let c = document.querySelector(".picture");
+        c.style.backgroundImage = 'url("./rightlookupwardsstanding.jpg")';
+
+       //reseting of picture
+        c.style.backgroundSize = "contain";
+        c.style.backgroundPosition = "center";
+
+}
+
+
+
 
 
     function replaceContent() {
@@ -69,3 +162,96 @@ initializeButton();
         b.style.backgroundSize = "contain";
         b.style.backgroundPosition = "center";
     }
+
+        function replaceContent4 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./leftlooklieflat.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent5 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./lookingupleft.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent6 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./lookingattreat.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent7 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./lookingatorangeleft.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent8 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./eatingorangeleft.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent9 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./leftlookupwards.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent10 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./facemirrorwithyeobo.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent11 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./inabox.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    function replaceContent12 () {
+        let b = document.querySelector(".picture");
+        b.style.backgroundImage = 'url("./frontlookupwards.jpg")';
+
+        // Optionally, you can also set other background properties like size and position
+        b.style.backgroundSize = "contain";
+        b.style.backgroundPosition = "center";
+    }
+
+    // Get the video element
+const video = document.getElementById("myVideo");
+const audio = document.getElementById("myAudio");
+
+window.onload = function () {
+// You can control the video using JavaScript, for example:
+video.play(); // Play the video
+audio.play();
+// Call the initializeButton function to set up the initial event listener
+initializeButton();
+}
